4abdf98e1215766cc294a5eb2c3943c126f4f996
Matthew Spence <mspence5555@gmail.com>
2022-01-15 13:32:50 -0600

    was able to display item in database to the DOM

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,15 @@
 const insertParagraph = require("../public/javascripts/insert-paragraph");
 const displayParagraph = require("../public/javascripts/display-paragraph");
+
 exports.index = function(req, res, next) {
     res.render('index', { title: 'Express' });
   };
 
 exports.display_paragraph = function(req, res, next) {
   
-  displayParagraph.displayParagraph();
-  res.redirect('/');
+  // displayParagraph.displayParagraph();
+  res.render('index', { ptext: displayParagraph.displayParagraph()});
+  // res.redirect('/');
 };
 
 // exports.submit_paragraph = function(req, res, next) {
--- public/javascripts/display-paragraph.js
+++ public/javascripts/display-paragraph.js
@@ -4,13 +4,9 @@ exports.displayParagraph = () => {
     
     const Database = require('better-sqlite3');
     const db = new Database('controllers/stories.db');
-    const new_paragraph = db.prepare('SELECT content FROM paragraphs WHERE id=1');
-    console.log(new_paragraph)
-    // let paragraph_div = document.getElementById("display_paragraph");
-    // let paragraph_text = document.createElement("p");
-    // paragraph_text.textContent = new_paragraph;
-    // paragraph_div.appendChild(paragraph_text);
-    
+    const current_paragraph = db.prepare('SELECT content FROM paragraphs WHERE id=1');
+    let p_value = current_paragraph.get();
     db.close();
+    return(p_value.content)
     
 };
