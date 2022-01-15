3bf0438773fc427339a632e4013d7363b3301795
Matthew Spence <mspence5555@gmail.com>
2022-01-15 16:23:15 -0600

    combined insert-paragraph.js and display-paragraph.js files into one name paragraph.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,13 @@
-const handle_paragraph = require("../public/javascripts/data/paragraph");
+const paragraph = require("../public/javascripts/data/paragraph");
 
 
 exports.index = function(req, res, next) {
-    res.render('index', { title: 'Express' });
-  };
-
-exports.display_paragraph = function(req, res, next) {
   
-  // handle_paragraph.handle_paragraph();
-  res.render('index', { ptext: handle_paragraph.displayParagraph()});
-  
+  res.render('index', { title: 'Express' });
 };
 
-// exports.submit_paragraph = function(req, res, next) {
-//   let paragraph = req.body.paragraph_input;
-//   insertParagraph.insertParagraph(paragraph);
-//   res.redirect('/');
-// };
+// When a new paragraph is added
+exports.handle_paragraph = function(req, res, next) {
+  paragraph.insertParagraph;
+  res.render('index', { ptext: paragraph.displayParagraph()});
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-let index = require('../controllers/index')
+let index = require('../controllers/index');
+
 /* GET home page. */
 router.get('/', index.index);
 /* POST home page. */
-router.post('/', index.display_paragraph);
-// router.post('/', index.submit_paragraph);
+router.post('/', index.handle_paragraph);
+
 
 module.exports = router;
