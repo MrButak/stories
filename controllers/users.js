051d16a248ac0c0706f63c843376e420beb2d26b
Matthew Spence <mspence5555@gmail.com>
2022-04-20 10:11:34 -0500

    updated db call to only accept ssl connection

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
+        require: true,
         rejectUnauthorized: false
     }
 });
