3bef1d6e106f73746cec6ff072508b16c6bd977f
Matthew Spence <mspence5555@gmail.com>
2022-04-20 15:37:21 -0500

    ...

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+const https = require("https");
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -37,6 +38,11 @@ app.use(session({
     // rejectUnauthorized: false
     // Insert express-session options here
 }));
+https
+  .createServer(app)
+  .listen(3000, ()=>{
+    console.log('server is runing at port 4000')
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -55,13 +61,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
